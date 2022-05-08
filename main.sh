32d5069b8b7e70d7931f33c7781e78e4b0e7e12c
Munesh Bandaru <munesh.nani@gmail.com>
2022-05-08 15:39:20 -0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,9 @@
+
+# This step processes the raw data files to create the processed data for modeling
+python extractData.py
+
+# Baseline models
+python baselineModels.py
+
+# CNN Modeling
+python cnn_Softmax_triplet.py
